4fe1481cd40927dc542e15097fe63f7bb633918c
jerin.jomy <jerin.jomy@sysfore.com>
2024-01-25 13:01:09 +0530

    mew method added in feature 1

--- Feature.cs
+++ Feature.cs
@@ -2,4 +2,7 @@ public class Feature1{
     public void FeatureOneMethod(string name){
         System.Console.WriteLine(name);
     }
+    public void FeatureOneMethod2(string name){
+        System.Console.WriteLine(name);
+    }
 }
