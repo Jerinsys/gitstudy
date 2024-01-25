5fb0c7d85ef54db1ccf80fc06229691651345222
jerin.jomy <jerin.jomy@sysfore.com>
2024-01-25 12:28:52 +0530

    added new feature

---
+++ Feature.cs
@@ -0,0 +1,5 @@
+public class Feature1{
+    public void FeatureOneMethod(string name){
+        System.Console.WriteLine(name);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+var fet = new Feature1();
+fet.FeatureOneMethod("jerin");
 void FristChange()
 {
     System.Console.WriteLine("first change");
